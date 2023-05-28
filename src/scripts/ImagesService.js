2c1ab705cdb483c57a7a6ff76060ff42f6bf3522
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2023-05-29 00:44:36 +0200

    HW11_v2

--- src/scripts/ImagesService.js
+++ src/scripts/ImagesService.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const URL = 'https://pixabay.com/api/';
 const API_KEY = '36837848-516d951f80548e8f5fb29462d';
@@ -7,25 +8,31 @@ export default class ImagesService {
   constructor() {
     this.page = 1;
     this.searchQuery = '';
+    this.totalHits = 0;
   }
 
-async getImages() {
-  const parameters = new URLSearchParams({
-    q: this.searchQuery,
-    per_page: 40,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
+  async getImages() {
+    const parameters = new URLSearchParams({
+      page: this.page,
+      q: this.searchQuery,
+      per_page: 40,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    });
 
-  const { data } = await axios.get(
-    `${URL}?key=${API_KEY}&${parameters}&page=${this.page}`
-  );
+    const response = await axios.get(`${URL}?key=${API_KEY}&${parameters}`);
 
+    if (this.page === 1) {
+      this.totalHits = response.data.totalHits;
+      if (this.totalHits !== 0) {
+        Notiflix.Notify.success(`Hooray! We found ${this.totalHits} images!`);
+      }
+    }
 
-  this.incrementPage();
-  return data.hits; }
-
+    this.incrementPage();
+    return response.data.hits;
+  }
 
   resetPage() {
     this.page = 1;
@@ -35,7 +42,3 @@ async getImages() {
     this.page += 1;
   }
 }
-
-
-
-
